aa4dd7610fd2428a428cd3333b1082fdb1c8cecd
gavinking <gavin@ceylon-lang.org>
2017-03-07 15:30:31 +0100

    minor

--- Application/src/main/ceylon/com/example/android/displayingbitmaps/ui/OnItemClickBase.java
+++ Application/src/main/ceylon/com/example/android/displayingbitmaps/ui/OnItemClickBase.java
@@ -1,14 +1,11 @@
 package com.example.android.displayingbitmaps.ui;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 
-/**
- * Created by gavin on 3/4/17.
- */
-
 public interface OnItemClickBase extends AdapterView.OnItemClickListener {
-    @Override
+    @Override @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     public void onItemClick(AdapterView adapterView, View view, int i, long l);
 }
